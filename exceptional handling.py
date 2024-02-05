6b9ba1984741ca4609cbd6165a016cc78acf84c4
legendryflyer <tavishanade12@gmail.com>
2024-02-05 21:16:54 +0530

    added

---
+++ exceptional handling.py
@@ -0,0 +1,62 @@
+#compile time error
+#runtime error 
+#logical error 
+
+# program 1 
+def add(x,y):
+    print(x+y)
+add(1,2)
+
+
+# program 2
+#x = input("Enter the number one")
+#y = input("Enter the number two")
+#print("hello")
+#print(int(x)/int(y))
+#print("bye")
+
+
+# program 3
+def calci(age):
+    return age
+a=calci(20)
+print(a)
+
+# program 4
+#print("hell")
+#print(10/0)
+#print("bye")
+
+
+
+
+print("hellow")
+try:
+    print(10/0)
+except Exception:
+    print("division by zero")
+print("hellow")
+
+
+
+print("shcjaef")
+try:
+    print(10/0)
+except Exception:
+    print("division  by  zero")
+else:
+    print("bye")
+finally:
+    print("tavish")
+
+
+
+
+
+
+
+
+
+
+
+
