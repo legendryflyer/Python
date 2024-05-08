ff544270d924074207adeb530201057a0e07622f
legendryflyer <tavish.anade@gmail.com>
2024-05-08 19:16:22 +0530

    added

--- regEx2.py
+++ regEx2.py
@@ -103,3 +103,123 @@ print(q4)
 # str = "three one two three four five six seven aa 8 9 10 two"
 # listD = re.findall(r'\At[\w]*',str)
 # print(listD)
+
+
+
+import re
+
+# \w [a-z][A-Z][0-9]
+# \b - blank space
+
+# program 1
+str = "an apple a day keeps doctor away"
+e = re.findall(r'\ba[\w]*',str)
+print(e)
+
+# program 2
+str2 = "The meeting will be conducted on 21st and 22nd and 1st"
+#[21st,22nd,1st]
+e2 = re.findall(r'\d[\w]*',str2)
+e3 = re.findall(r'\d[\d]*',str2)
+print(e2)
+print(e3)
+
+# program 3
+str3 = "one two three four five aa bb six seven 8 9 10 ad44566m "
+e4 = re.findall(r'\w{5}',str3)
+e5 = re.findall(r'\w{4,}',str3)
+e6 = re.findall(r'\w{3,5}',str3)
+print(e4)
+print(e5)
+print(e6)
+
+# program 4
+e7 = re.findall(r'\d{1,}',str3)
+e8 = re.findall(r'\b\d{1,}\b',str3)
+print(e7)
+print(e8)
+
+#program 5 
+str3 = "one two three four five six seven eight nine ten"
+e9 = re.findall(r't[\w]*\Z',str3)
+print(e9)
+e10 = re.findall(r'\Ao[\w]*',str3)
+print(e10)
+
+# program 6
+
+str4 = "ChinmayDeshpande:7709192441"
+e5 = re.search(r'\d+',str4)
+print(e5.group())
+
+#a[\w]*   #a apple a day keeps doctor away  
+#a\w+
+
+a = 'apple a day keeps doctor away' 
+print(re.findall(r'a\w+',a))
+print(re.findall(r'a[\w]*',a))
+
+
+
+
+
+
+
+
+
+
+
+# program 1
+import re
+str = "anil akhil anant an ak arun arati arundhanti abhjit ankur"
+e = re.findall(r'\ba[nk][\w]*\b',str)
+print(e)
+
+# program 2
+# dd-mm-yy
+
+str = 'chinmay 7-11-1989 ,amol 19-08-1990 mayuri 21-2-1990 poorva 29-10-93 shivani 27 april 1994'
+e = re.findall(r'\d{1,2}-\d{1,2}-\d{2,4}',str)
+d = re.findall(r'\d{2}-\d{2}-\d{4}',str)
+print(e)
+print(d)
+
+# program 3
+
+str = "Hello world"
+e = re.search(r'^he',str)
+if e:
+    print("string starts with He")
+else:
+    print("string does not starts with He")
+
+
+str = "Hello world .. i am from India"
+e = re.search(r'from India$',str)
+if e:
+    print("str ends with dia")
+else:
+    print("string does not end with dia")
+
+# program 4 
+str = "Hello WoRld"
+e = re.search(r"world$",str,re.IGNORECASE)
+if e :
+    print("ends with world")
+else:
+    print('does not end with world')
+
+# program 5
+str2 = "Rahul got 95 marks , Vijay got 89 marks Chinmay got 60 marks"
+f = re.findall(r'\d{1,2}',str2)
+print(f)
+
+g = re.findall(r'[A-Z][a-z]*',str2)
+print(g)
+
+# program 6
+str3 = "The meeting will start at either 9am or 9pm else at evening 6pm"
+
+# search - firstOccurence
+# match - start of string 
+# findall - all occurences
